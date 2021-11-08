aafbac8516a8c5319b20083e67d0b015663e1789
SuperOrteg <asseorteg@gmail.com>
2021-11-08 17:15:36 +0000

    Starting Action Mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+	after_create :welcome_send
 	has_many :admin_events, foreign_key: 'administrator_id', class_name: "Event"
 	has_many :attendances
 	has_many :events, through: :attendances
+
+	def welcome_send
+	    UserMailer.welcome_email(self).deliver_now
+	  end
 end
