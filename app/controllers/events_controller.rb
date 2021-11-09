2f15f542e35e46579dd966557a494e0f3f74e61f
SuperOrteg <asseorteg@gmail.com>
2021-11-09 13:06:12 +0000

    Adding new events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,59 @@
 class EventsController < ApplicationController
-	def index
-		@events = Event.all
-	end
+  #before_action :authenticate_user!, only: [:create, :new]
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def index
+    @event = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.administrator = current_user
+    if @event.save
+      flash.now[:alert] = "Félicitations ! L'événement a été créé !"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "L'événement n'a pas été créé ! Veuillez réessayer."
+      render new_event_path
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to event_path(params[:id])
+    else
+      flash.now[:alert] = "L'événement n'a pas été modifié ! Veuillez réessayer."
+      render edit_event_path
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      flash.now[:alert] = "Félicitations ! L'événement a été supprimé !"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "L'événement n'a pas été détruit ! Veuillez réessayer."
+      render event_path
+    end
+  end
+
+  private
+
+  def event_params
+    event_params = params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location)
+  end
+
 end
